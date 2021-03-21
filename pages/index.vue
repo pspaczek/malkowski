c8a0a0a1a79d42a38f7097339f71d07cc15c65c3
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2021-03-21 11:43:05 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="index">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup () {
+    const message = ref('')
+    return { message }
+  },
+  head: {}
+})
+</script>
+
+<style lang="scss">
+#index {
+  cursor: inherit;
+}
+</style>
